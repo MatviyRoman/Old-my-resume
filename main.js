8ecf7b45598236cceb6cb78959c3e6a931979a75
Roman <romas6ka@i.ua>
2018-11-07 13:25:13 +0200

    Add main.js
    Add function if show & hide phone number after click in number.

---
+++ main.js
@@ -0,0 +1,13 @@
+$(function(){
+	$('.phoneHide').click( function(){
+		$('.phoneHide').toggle();
+		$('.phoneShow').toggle();
+	})
+})
+
+$(function(){
+	$('.phoneShow').click( function(){
+		$('.phoneHide').toggle();
+		$('.phoneShow').toggle();
+	})
+})
